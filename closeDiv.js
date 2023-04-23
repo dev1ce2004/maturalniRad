30d35504a1f4087d8f6ccdb56c904efeef76ee98
dev1ce <105132637+dev1ce2004@users.noreply.github.com>
2023-04-23 19:43:42 +0200

    Add files via upload

---
+++ closeDiv.js
@@ -0,0 +1,3 @@
+function closeDiv() {
+    console.log('ante');
+}
---
+++ simple-contant.js
@@ -0,0 +1,3 @@
+let personnelDefault = document.getElementsByClassName('personnel-default');
+let personnelMore = document.getElementsByClassName('personnel-block-about');
+let counter = 0;
